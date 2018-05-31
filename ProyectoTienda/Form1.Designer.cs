182f7148f7658618df7393222a06e9bb98819ed7
Alumno <Alumno@WINDOWS7-PC>
2018-05-31 13:59:53 +0200

    Cambio

--- ProyectoTienda/Form1.Designer.cs
+++ ProyectoTienda/Form1.Designer.cs
@@ -117,7 +117,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(342, 258);
+            this.button1.Location = new System.Drawing.Point(432, 259);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 8;
