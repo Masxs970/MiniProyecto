d30f2c9f8435ad9777953ae1b5c194a90039777e
Alumno <Alumno@WINDOWS7-PC>
2018-05-31 13:43:07 +0200

    Prueba

--- ProyectoTienda/Form1.Designer.cs
+++ ProyectoTienda/Form1.Designer.cs
@@ -36,6 +36,7 @@
             this.ConectarButton = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.SalirButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -114,11 +115,21 @@
             this.SalirButton.UseVisualStyleBackColor = true;
             this.SalirButton.Click += new System.EventHandler(this.SalirButton_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(384, 269);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // LogIn
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(678, 331);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.SalirButton);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.ConectarButton);
@@ -145,6 +156,7 @@
         private System.Windows.Forms.Button ConectarButton;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button SalirButton;
+        private System.Windows.Forms.Button button1;
     }
 }
 
