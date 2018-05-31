3d5af726b9a73d22321f9471f58dee1e47bb1e4c
maynoboa <36697487+maynoboa@users.noreply.github.com>
2018-05-31 14:05:34 +0200

    mio

--- ProyectoTienda/Compras.Designer.cs
+++ ProyectoTienda/Compras.Designer.cs
@@ -35,6 +35,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.SalirCompras = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -97,11 +98,21 @@
             this.label2.TabIndex = 6;
             this.label2.Text = "Filtrar";
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(547, 152);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // ViewCompras
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(722, 294);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.SalirCompras);
             this.Controls.Add(this.button1);
@@ -126,5 +137,6 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button SalirCompras;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
     }
 }
