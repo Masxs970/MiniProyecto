83a10099259c4da4c2c82cd474a182649fb656fb
maynoboa <36697487+maynoboa@users.noreply.github.com>
2018-05-31 14:07:09 +0200

    pgf

--- ProyectoTienda/Compras.Designer.cs
+++ ProyectoTienda/Compras.Designer.cs
@@ -100,7 +100,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(547, 152);
+            this.button2.Location = new System.Drawing.Point(491, 153);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 7;
