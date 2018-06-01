e009419a58a86a245beda2ffe5c794d7919bb98b
Alumno <Alumno@WINDOWS7-PC>
2018-06-01 09:23:48 +0200

    ghy

--- ProyectoTienda/Clientes.Designer.cs
+++ ProyectoTienda/Clientes.Designer.cs
@@ -163,7 +163,7 @@
             this.emailDataGridViewTextBoxColumn,
             this.telefonoDataGridViewTextBoxColumn});
             this.dataGridView1.DataSource = this.clienteBindingSource;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 107);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 26);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(382, 198);
             this.dataGridView1.TabIndex = 12;
